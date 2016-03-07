c84482e2f01bdcbe2276c004bec4fb2306008052
Koutsiaris <koutsiarisc@chodevkoutsiarisc.local>
2016-03-07 10:37:29 +0000

    conflicts

---
+++ api/car/controllers/Car.js
@@ -0,0 +1,122 @@
+'use strict';
+
+const model = 'car';
+
+/**
+ * A set of functions called "actions" for `Car`
+ */
+
+module.exports = {
+
+  /**
+   * Get Car entries.
+   *
+   * @return {Object|Array}
+   */
+
+  find: function * () {
+    this.model = model;
+    try {
+      let entry = yield strapi.hooks.blueprints.find(this);
+      this.body = entry;
+    } catch (err) {
+      this.body = err;
+    }
+  },
+
+  /**
+   * Get a specific Car.
+   *
+   * @return {Object|Array}
+   */
+
+  findOne: function * () {
+    this.model = model;
+    try {
+      let entry = yield strapi.hooks.blueprints.findOne(this);
+      this.body = entry;
+    } catch (err) {
+      this.body = err;
+    }
+  },
+
+  /**
+   * Create a Car entry.
+   *
+   * @return {Object}
+   */
+
+  create: function * () {
+    this.model = model;
+    try {
+      let entry = yield strapi.hooks.blueprints.create(this);
+      this.body = entry;
+    } catch (err) {
+      this.body = err;
+    }
+  },
+
+  /**
+   * Update a Car entry.
+   *
+   * @return {Object}
+   */
+
+  update: function * () {
+    this.model = model;
+    try {
+      let entry = yield strapi.hooks.blueprints.update(this);
+      this.body = entry;
+    } catch (err) {
+      this.body = err;
+    }
+  },
+
+  /**
+   * Destroy a Car entry.
+   *
+   * @return {Object}
+   */
+
+  destroy: function * () {
+    this.model = model;
+    try {
+      let entry = yield strapi.hooks.blueprints.destroy(this);
+      this.body = entry;
+    } catch (err) {
+      this.body = err;
+    }
+  },
+
+  /**
+   * Add an entry to a specific Car.
+   *
+   * @return {Object}
+   */
+
+  add: function * () {
+    this.model = model;
+    try {
+      let entry = yield strapi.hooks.blueprints.add(this);
+      this.body = entry;
+    } catch (err) {
+      this.body = err;
+    }
+  },
+
+  /**
+   * Remove a specific entry from a specific Car.
+   *
+   * @return {Object}
+   */
+
+  remove: function * () {
+    this.model = model;
+    try {
+      let entry = yield strapi.hooks.blueprints.remove(this);
+      this.body = entry;
+    } catch (err) {
+      this.body = err;
+    }
+  }
+};
